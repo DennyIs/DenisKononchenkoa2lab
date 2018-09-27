bc94ce425a90fc2264e4577d3c17b62c01e57ccf
DennyIs <d.naglyadniy@gmail.com>
2018-09-27 12:35:11 +0300

    Лекция 1, задание 1 состоит из index.html,first.js,style.css

---
+++ first.js
@@ -0,0 +1,12 @@
+
+function hide_lesson() 
+    {
+  document.getElementById("hiden_lessons").style.display = "block";
+  document.getElementById("hiden_bells").style.display = "none"
+}
+
+
+function hide_bells() {
+    document.getElementById("hiden_bells").style.display = "block";
+    document.getElementById("hiden_lessons").style.display = "none"
+}
