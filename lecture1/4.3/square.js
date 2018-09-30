9ba35e4466c20d96a4af7bbb9ddfe1c4add97271
DennyIs <d.naglyadniy@gmail.com>
2018-09-30 12:34:41 +0300

    Исправил

--- lecture1/4.3/square.js
+++ lecture1/4.3/square.js
@@ -1,31 +1,25 @@
 function square_equation() {
-  var a = document.square.square_a.value;
-  var b = document.square.square_b.value;
-  var c = document.square.square_c.value;
-
-  var d = b * b - 4 * a * c;
-  if ( d < 0 ) {
-    string = "Комплексно сопраженные корня<br>x<sub>1</sub> = (";
-    string += - b / ( 2 * a );
-    string += ", ";
-    string += Math.sqrt( -d ) / ( 2 * a );
-    string += "), x<sub>2</sub> = (";
-    string += - b / ( 2 * a);
-    string += ", ";
-    string += - Math.sqrt( -d ) / ( 2 * a );
-    string += ").";
-  } else {
-      if ( d == 0 ) {
-        string = "Два одинаковых вещественных корня:<br>x<sub>1</sub> = x<sub>2</sub> = ";
-        string += -b / ( 2 * a );
-        string += ".";
-      } else {
-        string = "Два различных вещественных корня:<br>x<sub>1</sub> = ";
-        string += -b / ( 2 * a ) - Math.sqrt( d ) / ( 2 * a );
-        string += ", x<sub>2</sub> = ";
-        string += -b / ( 2 * a ) + Math.sqrt( d ) / ( 2 * a );
-        string += ".";
-      }
-  }
-  document.getElementById("square_sol").innerHTML = string;
+    var a = document.square.square_a.value;
+    var b = document.square.square_b.value;
+    var c = document.square.square_c.value;
+  
+    var d = b * b - 4 * a * c;  
+   var x1, x2;
+    if ( d < 0 ) {
+      
+      x1 = - b / ( 2 * a );
+      x1 = Math.sqrt( -d ) / ( 2 * a );
+      x2 = - b / ( 2 * a);
+      x2 = - Math.sqrt( -d ) / ( 2 * a );
+    } else {
+        if ( d == 0 ) {
+          x1, x2 = -b / ( 2 * a );
+        } else {
+          x1 = -b / ( 2 * a ) - Math.sqrt( d ) / ( 2 * a );
+          x2 = -b / ( 2 * a ) + Math.sqrt( d ) / ( 2 * a );
+          
+        }
+    }
+    document.getElementById("square_sol_x1").innerHTML = x1;
+    document.getElementById("square_sol_x2").innerHTML = x2;
 }
