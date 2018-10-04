bf4ace1818848a4be0273bc3788fa5aec86dd073
DennyIs <d.naglyadniy@gmail.com>
2018-10-04 10:33:40 +0300

    Caesar's cipher fixed, now when you enter the letter z, the letter a appears. Vigenera cipher implemented

--- lecture2/2.1/CeasarsCipher.js
+++ lecture2/2.1/CeasarsCipher.js
@@ -8,8 +8,13 @@ function CeasarEncryptor(){
     {
         letter = message[i]
         letter = letter.charCodeAt(letter)
-        letter = letter + shift
-        result += String.fromCharCode(letter)
+        if (letter >= 97 && letter <=122) {
+            result += String.fromCharCode((letter - 97 + shift) % 26 + 97)
+        } else if(letter >= 65 && letter <=90){
+            result += String.fromCharCode((letter - 65 + shift) % 26 + 65)
+        } else {
+            result += String.fromCharCode(letter)
+        }
         i++
     }
     document.getElementById("output").value = result
@@ -18,16 +23,20 @@ function CeasarEncryptor(){
 function CeasarDecryptor(){
 	output = document.getElementById("output").value
     shift = document.getElementById("shift").value
-	decryptor = CeasarEncryptor()
 	var decryp_result = ""
 	shift =- shift
     var i = 0
     while (i < output.length)
-    {
+    {   
     	letter = output[i]
     	letter = letter.charCodeAt(letter)
-    	letter = letter + shift
-    	decryp_result += String.fromCharCode(letter)
+    	if (letter >=97 && letter <=122) {
+            decryp_result += String.fromCharCode((letter - 97 + shift + 26) % 26 + 97)
+        } else if (letter >= 65 && letter <= 90) {
+            decryp_result += String.fromCharCode((letter - 65 + shift + 26) % 26 + 65)
+        } else {
+            decryp_result += String.fromCharCode(letter)
+        }
     	i++
     }
     document.getElementById("output").value = decryp_result
---
+++ lecture2/2.2/VejenerCipher.js
@@ -0,0 +1,37 @@
+function VijenerEncryptor(){
+	message = document.getElementById("message").value.toLowerCase();
+    key = document.getElementById("key").value.toLowerCase();
+    output = message.split('');
+    var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    var result = "";
+    var index = 0;
+    output.forEach(item => {
+        var temp = (alphabet.indexOf(item) + alphabet.length + alphabet.indexOf(key[index])) % alphabet.length;
+        result += alphabet[temp]
+        index += 1
+        if (index + 1 > key.length) {
+            index = 0
+        }
+    });
+    console.log(result)
+    document.getElementById("output").value = result
+    
+}
+function VijenerDecryptor() {
+    output = document.getElementById("output").value.toLowerCase();
+    key = document.getElementById("key").value.toLowerCase();
+    document.getElementById("message").value = output;
+    output = output.split('');
+    var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    var result = "";
+    var index = 0;
+    output.forEach(item => {
+        var temp = (alphabet.indexOf(item) + alphabet.length - alphabet.indexOf(key[index])) % alphabet.length;
+        result += alphabet[temp]
+        index += 1
+        if (index + 1 > key.length) {
+            index = 0
+        }
+    });
+    document.getElementById("output").value = result
+}
